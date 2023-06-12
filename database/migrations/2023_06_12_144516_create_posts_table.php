82e28918f5f13a1cd0716222e37bb48b3aa4a98f
unknown <shafayat.h.b@gmail.com>
2023-06-12 21:05:10 +0600

    OSTAD PHP Laravel - Assignment Module 17

--- database/migrations/2023_06_12_144516_create_posts_table.php
+++ database/migrations/2023_06_12_144516_create_posts_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('title', 100);
+            $table->string('slug', 150);
+            $table->string('excerpt', 150);
+            $table->longText('description');
+            $table->boolean('is_published');
+            $table->string('min_to_read', 10);
+
+            
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('posts');
     }
 };
